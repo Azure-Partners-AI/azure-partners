0755353d12a22d1bdd8537a8f3b591c354270046
Tom Zhang <tommyawesome08@gmail.com>
2025-06-04 22:39:09 -0400

    fixed the navigation items to the proper ones

--- frontend/data/navigationZH.tsx
+++ frontend/data/navigationZH.tsx
@@ -1,26 +1,40 @@
-const chinese = 'zh'
+const chinese = "zh";
 
 export const mainItems = [
-    { title: "Programs", route: `${chinese}/programs` },
-    { title: "About", route: `${chinese}/about` },
-    { title: "Events", route: `${chinese}/events` },
-    { title: "Offerings", route: `${chinese}/offerings` },
-]
+  { title: "Programs", route: `${chinese}/programs` },
+  { title: "About", route: `${chinese}/about` },
+  { title: "Events", route: `${chinese}/events` },
+  { title: "Offerings", route: `${chinese}/offerings` },
+];
 
 export const programItems = [
-    { title: "AI for VC", route: `${chinese}/programs/ai-for-vc` },
-    { title: "AI for Finance", route: `${chinese}/programs/ai-for-finance` },
-    { title: "Ai for Education", route: `${chinese}/programs/ai-for-education` },
-]
+  { title: "AI for VC", route: `${chinese}/programs/ai-for-vc` },
+  { title: "AI for Finance", route: `${chinese}/programs/ai-for-finance` },
+  { title: "Ai for Education", route: `${chinese}/programs/ai-for-education` },
+];
 
 export const aboutItems = [
-    { title: "LinkedIn + WengZong", route: `${chinese}/about/linkedin-wengzong` },
-]
+  { title: "Company", route: `${chinese}/about/company` },
+  { title: "CEO & Co-Founder", route: `${chinese}/about/ceo-and-co-founder` },
+];
+
+export const eventItems = [
+  { title: "LinkedIn + WengZong", route: `${chinese}/about/linkedin-wengzong` },
+];
 
 export const offeringsItems = [
-    { title: "Courses", route: `${chinese}/offerings/courses` },
-    { title: "Internship", route: `${chinese}/offerings/internship` },
-    { title: "AI Tutoring Services", route: `${chinese}/offerings/ai-tutoring-services` },
-    { title: "Content Image/Video Generation", route: `${chinese}/offerings/content-generation` },
-    { title: "AI Application Development", route: `${chinese}/offerings/ai-application-development` },
-]
+  { title: "Courses", route: `${chinese}/offerings/courses` },
+  { title: "Internship", route: `${chinese}/offerings/internship` },
+  {
+    title: "AI Tutoring Services",
+    route: `${chinese}/offerings/ai-tutoring-services`,
+  },
+  {
+    title: "Content Image/Video Generation",
+    route: `${chinese}/offerings/content-generation`,
+  },
+  {
+    title: "AI Application Development",
+    route: `${chinese}/offerings/ai-application-development`,
+  },
+];
