1d32129cc13f44c35d1d614bbc6c4e81c1fddb3a
Tom Zhang <tommyawesome08@gmail.com>
2025-06-04 22:31:20 -0400

    ENG + ZH navigation items

---
+++ frontend/data/navigationEN.tsx
@@ -0,0 +1,26 @@
+const english = 'en'
+
+export const mainItems = [
+    { title: "Programs", route: `${english}/programs` },
+    { title: "About", route: `${english}/about` },
+    { title: "Events", route: `${english}/events` },
+    { title: "Offerings", route: `${english}/offerings` },
+]
+
+export const programItems = [
+    { title: "AI for VC", route: `${english}/programs/ai-for-vc` },
+    { title: "AI for Finance", route: `${english}/programs/ai-for-finance` },
+    { title: "Ai for Education", route: `${english}/programs/ai-for-education` },
+]
+
+export const aboutItems = [
+    { title: "LinkedIn + WengZong", route: `${english}/about/linkedin-wengzong` },
+]
+
+export const offeringsItems = [
+    { title: "Courses", route: `${english}/offerings/courses` },
+    { title: "Internship", route: `${english}/offerings/internship` },
+    { title: "AI Tutoring Services", route: `${english}/offerings/ai-tutoring-services` },
+    { title: "Content Image/Video Generation", route: `${english}/offerings/content-generation` },
+    { title: "AI Application Development", route: `${english}/offerings/ai-application-development` },
+]
---
+++ frontend/data/navigationZH.tsx
@@ -0,0 +1,26 @@
+const chinese = 'zh'
+
+export const mainItems = [
+    { title: "Programs", route: `${chinese}/programs` },
+    { title: "About", route: `${chinese}/about` },
+    { title: "Events", route: `${chinese}/events` },
+    { title: "Offerings", route: `${chinese}/offerings` },
+]
+
+export const programItems = [
+    { title: "AI for VC", route: `${chinese}/programs/ai-for-vc` },
+    { title: "AI for Finance", route: `${chinese}/programs/ai-for-finance` },
+    { title: "Ai for Education", route: `${chinese}/programs/ai-for-education` },
+]
+
+export const aboutItems = [
+    { title: "LinkedIn + WengZong", route: `${chinese}/about/linkedin-wengzong` },
+]
+
+export const offeringsItems = [
+    { title: "Courses", route: `${chinese}/offerings/courses` },
+    { title: "Internship", route: `${chinese}/offerings/internship` },
+    { title: "AI Tutoring Services", route: `${chinese}/offerings/ai-tutoring-services` },
+    { title: "Content Image/Video Generation", route: `${chinese}/offerings/content-generation` },
+    { title: "AI Application Development", route: `${chinese}/offerings/ai-application-development` },
+]
